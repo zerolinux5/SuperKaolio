679a5751cb632a9f40fc8d15cf11055b953e8a96
Jesus Magana <maganazero5@gmail.com>
2014-07-03 16:13:47 -0700

    Added GameOver Screen and die from hazards

--- SuperKoalio/GameLevelScene.m
+++ SuperKoalio/GameLevelScene.m
@@ -16,6 +16,8 @@
 @property (nonatomic, strong) Player *player;
 @property (nonatomic, assign) NSTimeInterval previousUpdateTime;
 @property (nonatomic, strong) TMXLayer *walls;
+@property (nonatomic, strong) TMXLayer *hazards;
+@property (nonatomic, assign) BOOL gameOver;
 @end
 
 @implementation GameLevelScene
@@ -30,6 +32,8 @@
     [self addChild:self.map];
     
     self.walls = [self.map layerNamed:@"walls"];
+    
+    self.hazards = [self.map layerNamed:@"hazards"];
     
     self.player = [[Player alloc] initWithImageNamed:@"koalio_stand"];
     self.player.position = CGPointMake(100, 50);
@@ -54,6 +58,8 @@
 //1
 - (void)update:(NSTimeInterval)currentTime
 {
+  if (self.gameOver) return;
+  
   [self setViewpointCenter:self.player.position];
   //2
   NSTimeInterval delta = currentTime - self.previousUpdateTime;
@@ -67,6 +73,7 @@
   [self.player update:delta];
   
   [self checkForAndResolveCollisionsForPlayer:self.player forLayer:self.walls];
+  [self handleHazardCollisions:self.player];
 }
 
 - (void)checkForAndResolveCollisionsForPlayer:(Player *)player forLayer:(TMXLayer *)layer
@@ -190,6 +197,66 @@
   CGPoint centerOfView = CGPointMake(self.size.width/2, self.size.height/2);
   CGPoint viewPoint = CGPointSubtract(centerOfView, actualPosition);
   self.map.position = viewPoint;
+}
+
+- (void)handleHazardCollisions:(Player *)player
+{
+  NSInteger indices[8] = {7, 1, 3, 5, 0, 2, 6, 8};
+  
+  for (NSUInteger i = 0; i < 8; i++) {
+    NSInteger tileIndex = indices[i];
+    
+    CGRect playerRect = [player collisionBoundingBox];
+    CGPoint playerCoord = [self.hazards coordForPoint:player.desiredPosition];
+    
+    NSInteger tileColumn = tileIndex % 3;
+    NSInteger tileRow = tileIndex / 3;
+    CGPoint tileCoord = CGPointMake(playerCoord.x + (tileColumn - 1), playerCoord.y + (tileRow - 1));
+    
+    NSInteger gid = [self tileGIDAtTileCoord:tileCoord forLayer:self.hazards];
+    if (gid != 0) {
+      CGRect tileRect = [self tileRectFromTileCoords:tileCoord];
+      if (CGRectIntersectsRect(playerRect, tileRect)) {
+        [self gameOver:0];
+      }
+    }
+  }
+}
+
+-(void)gameOver:(BOOL)won {
+  //1
+  self.gameOver = YES;
+  //2
+  NSString *gameText;
+  if (won) {
+    gameText = @"You Won!";
+  } else {
+    gameText = @"You have Died!";
+  }
+  
+  //3
+  SKLabelNode *endGameLabel = [SKLabelNode labelNodeWithFontNamed:@"Marker Felt"];
+  endGameLabel.text = gameText;
+  endGameLabel.fontSize = 40;
+  endGameLabel.position = CGPointMake(self.size.width / 2.0, self.size.height / 1.7);
+  [self addChild:endGameLabel];
+  
+  //4
+  UIButton *replay = [UIButton buttonWithType:UIButtonTypeCustom];
+  replay.tag = 321;
+  UIImage *replayImage = [UIImage imageNamed:@"replay"];
+  [replay setImage:replayImage forState:UIControlStateNormal];
+  [replay addTarget:self action:@selector(replay:) forControlEvents:UIControlEventTouchUpInside];
+  replay.frame = CGRectMake(self.size.width / 2.0 - replayImage.size.width / 2.0, self.size.height / 2.0 - replayImage.size.height / 2.0, replayImage.size.width, replayImage.size.height);
+  [self.view addSubview:replay];
+}
+
+- (void)replay:(id)sender
+{
+  //5
+  [[self.view viewWithTag:321] removeFromSuperview];
+  //6
+  [self.view presentScene:[[GameLevelScene alloc] initWithSize:self.size]];
 }
 
 @end
