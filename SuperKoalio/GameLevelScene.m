a95271f111f7d04b28fe54fa3f22a55e2e145df5
Jesus Magana <maganazero5@gmail.com>
2014-07-03 13:49:16 -0700

    Added koala and level

--- SuperKoalio/GameLevelScene.m
+++ SuperKoalio/GameLevelScene.m
@@ -7,12 +7,28 @@
 //
 
 #import "GameLevelScene.h"
+#import "JSTileMap.h"
+#import "Player.h"
+
+@interface GameLevelScene()
+@property (nonatomic, strong) JSTileMap *map;
+@property (nonatomic, strong) Player *player;
+@end
 
 @implementation GameLevelScene
 
 -(id)initWithSize:(CGSize)size {
   if (self = [super initWithSize:size]) {
     /* Setup your scene here */
+    self.backgroundColor = [SKColor colorWithRed:.4 green:.4 blue:.95 alpha:1.0];
+    
+    self.map = [JSTileMap mapNamed:@"level1.tmx"];
+    [self addChild:self.map];
+    
+    self.player = [[Player alloc] initWithImageNamed:@"koalio_stand"];
+    self.player.position = CGPointMake(100, 50);
+    self.player.zPosition = 15;
+    [self.map addChild:self.player];
     
   }
   return self;
